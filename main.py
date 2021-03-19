eed9f4c3c52bb49aac2681542bbf1c46ea186b31
Farshad Rahimi Ghashghaei <73135303+farshadrgh@users.noreply.github.com>
2021-03-18 21:26:22 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def file_lengthy(FileName):
+def FileLength(FileName):
     with open(FileName) as OurFile:
         for Number, Line in enumerate(OurFile):
             pass
     return Number + 1
 
 
-print("Number of lines in your file is: {}".format(file_lengthy("OurText.txt")))
+print("Number of lines in your file is: {}".format(FileLength("OurText.txt")))
